3866b16c2eb1770a430a6cf02b7757d0d92453d0
Leo Martinez <leo@Leos-iMac.local>
2019-01-16 16:50:05 -0500

    update

--- bs_radio.sh
+++ bs_radio.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
-echo "<div class=\"form-check\">"
-echo "  <input class=\"form-check-input\" type=\"radio\" name=\"$1\" id=\"$1Radios1\" value=\"option1\" checked>"
-echo "  <label class=\"form-check-label\" for=\"$1Radios1\">"
-echo "    Default radio"
-echo "   </label>"
-echo "</div>"
-echo "<div class=\"form-check\">"
-echo "  <input class=\"form-check-input\" type=\"radio\" name=\"$1\" id=\"$1Radios2\" value=\"option2\">"
-echo "  <label class=\"form-check-label\" for=\"$1Radios2\">"
-echo "    Second default radio"
-echo "  </label>"
-echo "</div>"
-echo "<div class=\"form-check disabled\">"
-echo "  <input class=\"form-check-input\" type=\"radio\" name=\"$1\" id=\"$1Radios3\" value=\"option3\" disabled>"
-echo "  <label class=\"form-check-label\" for=\"$1Radios3\">"
-echo "    Disabled radio"
-echo "  </label>"
-echo "</div>"
+data="<div class=\"form-check\">\n
+\t<input class=\"form-check-input\" type=\"radio\" name=\"$1\" id=\"$1Radios1\" value=\"option1\" checked>\n
+\t<label class=\"form-check-label\" for=\"$1Radios1\">\n
+\t\t\tDefault radio\n
+\t</label>\n
+</div>\n
+<div class=\"form-check\">\n
+\t<input class=\"form-check-input\" type=\"radio\" name=\"$1\" id=\"$1Radios2\" value=\"option2\">\n
+\t<label class=\"form-check-label\" for=\"$1Radios2\">\n
+\t\t\tSecond default radio\n
+\t</label>\n
+</div>\n
+<div class=\"form-check disabled\">\n
+\t<input class=\"form-check-input\" type=\"radio\" name=\"$1\" id=\"$1Radios3\" value=\"option3\" disabled>\n
+\t<label class=\"form-check-label\" for=\"$1Radios3\">\n
+\t\t\\tDisabled radio\n
+\t</label>\n
+</div>\n"
+
+echo $data
